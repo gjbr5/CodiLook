bf96bcd3d303231550d883ed144e8012c3459a4c
lja <kelly0313@naver.com>
2020-08-01 22:18:59 +0900

    ui

--- app/src/main/java/kr/ceo/codilook/MainActivity.java
+++ app/src/main/java/kr/ceo/codilook/MainActivity.java
@@ -1,14 +1,24 @@
 package kr.ceo.codilook;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+        @Override
+          public void onClick (View view)  {
+          Intent intent1 = new Intent(getApplicationContext(), joinActivity2.class);
+          startActivity(intent1);
+        }
+        });
     }
 }
